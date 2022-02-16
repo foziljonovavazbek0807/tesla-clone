265613ca14d6a943f339ff5395ef52a19487c76a
Avazbek <foziljonovavazbek0807@gmail.com>
2022-02-17 04:43:42 +0500

    final simple

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../header.css";
+import { HiMenu } from "react-icons/hi";
+import { BiX } from "react-icons/bi";
+import styled from "styled-components";
 
 function Header() {
+
+  const [burgerStatus, setBurgerStatus] = useState(false)
+
   return (
-    <div className='Header'>
-      Header
+    <div className="Header">
+      <a href="#!">
+        <img src="../images/logo.png" alt="logo" style={{ width: "120px" }} />
+      </a>
+
+      <ul className="Menu">
+        <li>
+          <a href="#!">MODEL S</a>
+        </li>
+        <li>
+          <a href="#!">MODEL 3</a>
+        </li>
+        <li>
+          <a href="#!">MODEL X</a>
+        </li>
+        <li>
+          <a href="#!">MODEL Y</a>
+        </li>
+      </ul>
+
+      <ul className="SideMenu">
+        <li>
+          <a href="#!">SHOP</a>
+        </li>
+        <li>
+          <a href="#!">TESLA ACCOUNT</a>
+        </li>
+        <HiMenu className="MenuIcon" onClick={()=>setBurgerStatus(true)}/>
+      </ul>
+
+      <BurgerNav show={burgerStatus}>
+        <div className="BurgerX">
+          <BiX className="BtnX" onClick={()=>setBurgerStatus(false)}/>
+        </div>
+        <li className="BurgerItem">
+          <a href="#!">Model S</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Model 3</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Model X</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Model Y</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Existing Inventory</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Used Inventory</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Trade-in</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Cybertruck</a>
+        </li>
+        <li className="BurgerItem">
+          <a href="#!">Roadster</a>
+        </li>
+      
+      </BurgerNav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
+
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 150;
+    background-color: #fff;
+    width: 300px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+    transition: 0.5s;
+`
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../section.css";
 import { FaChevronDown } from "react-icons/fa";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 function Section({
   title,
@@ -12,17 +13,21 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
       <div className="ItemText">
         <h1>{title}</h1>
         <p>{description}</p>
       </div>
+      </Fade>
       <div className="Buttons">
+        <Fade bottom>
         <div className="ButtonGroup">
           <button className="BtnLeft">{leftBtnText}</button>
           {rightBtnText &&
           <button className="BtnRight">{rightBtnText}</button>
           }
         </div>
+        </Fade>
         <FaChevronDown className="DownArrow" />
       </div>
     </Wrap>
